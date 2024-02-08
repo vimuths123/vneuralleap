212e4304e9ef0b469393b71d00520c0a5d503871
vimuths123 <vimuths@yahoo.com>
2024-02-08 11:52:10 +0530

    Add onetime

---
+++ server/create-onetime-session.js
@@ -0,0 +1,45 @@
+
+require('dotenv').config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const success_url = "https://www.neuralleap.ai/thank-you.html";
+const cancel_url = "https://www.neuralleap.ai/thank-you.html";
+
+exports.handler = async (event, context) => {
+    const requestBody = JSON.parse(event.body);
+    const { priceid } = requestBody;
+
+    try {
+        const customer = await stripe.customers.create();
+
+        const session = await stripe.checkout.sessions.create({
+            customer: customer.id,
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    price: priceid,
+                    quantity: 1,
+                },
+            ],
+            mode: 'payment',
+            success_url: `${YOUR_DOMAIN}/success.html`,
+            cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                url: session.url,
+            }),
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: e.message,
+            }),
+        };
+    }
+};
+
